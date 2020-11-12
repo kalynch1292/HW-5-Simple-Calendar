d3c6b0067d4a51349b79dc4c337c372b58c6aac7
Kayla Lynch <kalynch1292@gmail.com>
2020-11-11 19:17:46 -0500

    final push

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HW-5-Simple-Calendar
-Simple JQUERY, Bootstrap, Javascript, HTML; CSS
+Create a simple Calendar with JQUERY. Saves to local storage and reflects current time.
+
+
+## Image of deployed site
+![screenshot](./assets/screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,6 @@
 $(document).ready(function () {
-    // // each hour needs a variable to get from local storage //
-
-    // var nineAl = localStorage.getItem("hour9");
-    // var tenAl = localStorage.getItem("hour10");
-    // var elevenAl = localStorage.getItem("hour11");
-    // var noonl = localStorage.getItem("hour12");
-    // var onePl = localStorage.getItem("hour13");
-    // var twoPl = localStorage.getItem("hour14");
-    // var threePl = localStorage.getItem("hour15");
-    // var fourPl = localStorage.getItem("hour16");
-    // var fivePl = localStorage.getItem("hour17");
-
-    // each div needs an id to use the L.S//
-
-    // probably could use an array for these//
-    // .data gives numerical value to div//
-
-
+    var todaysDate = moment().format('MMM Do YYYY');
+        $("#currentDay").text(todaysDate)
 
 
 
@@ -41,7 +25,6 @@ $(document).ready(function () {
     changeTimeBlocks();
     $(".saveBtn").on("click", function () {
         var hour = $(this).parent().attr("data-hour");
-        console.log(hour)
         var text = $("#hour" + hour).children(".description").val().trim()
         localStorage.setItem("text" + hour, text)
     })
